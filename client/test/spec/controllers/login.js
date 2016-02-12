7176252f9ca674704336c4e9d9a794a2d1ac39e8
bsmukasa <bsmukasa@gmail.com>
2016-02-12 01:57:03 -0500

    Adding test skeletons.

--- client/test/spec/controllers/login.js
+++ client/test/spec/controllers/login.js
@@ -1,23 +1,23 @@
 'use strict';
 
-describe('Controller: SignupCtrl', function () {
+describe('Controller: LoginCtrl', function () {
 
   // load the controller's module
   beforeEach(module('clientApp'));
 
-  var SignupCtrl,
+  var LoginCtrl,
     scope;
 
   // Initialize the controller and a mock scope
   beforeEach(inject(function ($controller, $rootScope) {
     scope = $rootScope.$new();
-    SignupCtrl = $controller('SignupCtrl', {
+    LoginCtrl = $controller('LoginCtrl', {
       $scope: scope
       // place here mocked dependencies
     });
   }));
 
   it('should attach a list of awesomeThings to the scope', function () {
-    expect(SignupCtrl.awesomeThings.length).toBe(3);
+    expect(LoginCtrl.awesomeThings.length).toBe(3);
   });
 });
