630f86f36b736fbec36e60527b9b41a1fc5981b1
bsmukasa <bsmukasa@gmail.com>
2016-02-11 22:26:06 -0500

    Fixes to signup on angular side.

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -24,10 +24,9 @@ angular
         controller: 'MainCtrl',
         controllerAs: 'main'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
+      .when('/signup', {
+        templateUrl: 'views/signup.html',
+        controller: 'SignupCtrl'
       })
       .otherwise({
         redirectTo: '/'
--- client/app/scripts/controllers/signup.js
+++ client/app/scripts/controllers/signup.js
@@ -1,17 +1,17 @@
 'use strict';
 
-/**
- * @ngdoc function
- * @name clientApp.controller:SignupCtrl
- * @description
- * # SignupCtrl
- * Controller of the clientApp
- */
 angular.module('clientApp')
-  .controller('SignupCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('SignupCtrl', function ($scope, $http, $log) {
+    $scope.signup = function() {
+      var payload = {
+        email : $scope.email,
+        password : $scope.password
+      };
+
+      $http.post('app/signup', payload)
+        .success(function(data) {
+          //console.log(data);
+          $log.debug(data);
+        });
+    };
   });
