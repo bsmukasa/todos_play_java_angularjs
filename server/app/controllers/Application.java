52f6aefc798761274274c4927a16808056df7a8b
bsmukasa <bsmukasa@gmail.com>
2016-02-13 07:11:29 -0500

    Update Application controller.

--- server/app/controllers/Application.java
+++ server/app/controllers/Application.java
@@ -1,10 +1,6 @@
 package controllers;
 
 import com.fasterxml.jackson.databind.node.ObjectNode;
-import models.User;
-import play.*;
-import play.data.Form;
-import play.data.validation.Constraints;
 import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Result;
@@ -17,39 +13,39 @@ public class Application extends Controller {
         return ok(index.render("Your new application is ready."));
     }
 
-    public Result signup() {
-        Form<SignUp> signUpForm = Form.form(SignUp.class).bindFromRequest();
-
-        if (signUpForm.hasErrors()) {
-            return badRequest(signUpForm.errorsAsJson());
-        }
-
-        SignUp newUser = signUpForm.get();
-        User existingUser = User.findByEmail(newUser.email);
-
-        if(existingUser != null) {
-            return badRequest(buildJsonResponse("error", "User exists"));
-        } else {
-            User user = new User();
-            user.setEmail(newUser.email);
-            user.setPassword(newUser.password);
-            user.save();
-
-            return ok(buildJsonResponse("success", "User created successfully"));
-        }
-    }
-
-    public static class UserForm {
-        @Constraints.Required
-        @Constraints.Email
-        public String email;
-    }
-
-    public static class SignUp extends UserForm {
-        @Constraints.Required
-        @Constraints.MinLength(6)
-        public String password;
-    }
+//    public Result signup() {
+//        Form<SignUp> signUpForm = Form.form(SignUp.class).bindFromRequest();
+//
+//        if (signUpForm.hasErrors()) {
+//            return badRequest(signUpForm.errorsAsJson());
+//        }
+//
+//        SignUp newUser = signUpForm.get();
+//        User existingUser = User.findByEmail(newUser.email);
+//
+//        if(existingUser != null) {
+//            return badRequest(buildJsonResponse("error", "User exists"));
+//        } else {
+//            User user = new User();
+//            user.setEmail(newUser.email);
+//            user.setPassword(newUser.password);
+//            user.save();
+//
+//            return ok(buildJsonResponse("success", "User created successfully"));
+//        }
+//    }
+//
+//    public static class UserForm {
+//        @Constraints.Required
+//        @Constraints.Email
+//        public String email;
+//    }
+//
+//    public static class SignUp extends UserForm {
+//        @Constraints.Required
+//        @Constraints.MinLength(6)
+//        public String password;
+//    }
 
     public static ObjectNode buildJsonResponse(String type, String message) {
         ObjectNode wrapper = Json.newObject();
