78ae20cac6f5028e01ac22e3a19fd22549dc0dd2
bsmukasa <bsmukasa@gmail.com>
2016-02-13 07:10:56 -0500

    Update User model.

--- server/app/models/User.java
+++ server/app/models/User.java
@@ -3,16 +3,13 @@ package models;
 import com.avaje.ebean.Model;
 import play.data.validation.Constraints;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User extends Model {
     @Id
+    @GeneratedValue
     public Long id;
 
     @Column(length = 255, unique = true, nullable = false)
@@ -21,24 +18,29 @@ public class User extends Model {
     public String email;
 
     @Column(length = 64, nullable = false)
-    private byte[] shaPassword;
+    public String password;
 
-    public void setPassword(String password) {
-        this.shaPassword = getSha512(password);
-    }
+//    @Column(length = 64, nullable = false)
+//    private byte[] shaPassword;
 
-    public void setEmail(String email) {
-        this.email = email.toLowerCase();
-    }
+    @Column(length = 255, nullable = false)
+    @Constraints.Required
+    public String firstName;
+
+    @Column(length = 255, nullable = false)
+    @Constraints.Required
+    public String lastName;
+
+//    public void setPassword(String password) {
+//        this.shaPassword = getSha512(password);
+//    }
+
+    // TODO Consider adding validation function.
 
     public static final Finder<Long, User> find = new Finder<>(User.class);
 
-    public static User findByEmailAndPassword(String email, String password) {
-        return find
-                .where()
-                .eq("email", email.toLowerCase())
-                .eq("shaPassword", getSha512(password))
-                .findUnique();
+    public static User authenticate(String email, String password) {
+        return find.where().eq("email", email).eq("password", password).findUnique();
     }
 
     public static User findByEmail(String email) {
@@ -48,15 +50,12 @@ public class User extends Model {
                 .findUnique();
     }
 
-    public static byte[] getSha512(String value) {
-        try {
-            return MessageDigest.getInstance("SHA-512").digest(value.getBytes("UTF-8"));
-        }
-        catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        }
-        catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public static byte[] getSha512(String value) {
+//        try {
+//            return MessageDigest.getInstance("SHA-512").digest(value.getBytes("UTF-8"));
+//        }
+//        catch (NoSuchAlgorithmException | UnsupportedEncodingException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
