1b354ea355eb60718d053cf4852d56c323822206
bsmukasa <bsmukasa@gmail.com>
2016-02-14 16:24:55 -0500

    Minor fix.

--- server/test/models/BaseModelTest.java
+++ server/test/models/BaseModelTest.java
@@ -19,34 +19,6 @@ public class BaseModelTest {
         Helpers.start(app);
 
         Ebean.save((List<?>) Yaml.load("test-data.yml"));
-
-//        User user1 = new User();
-//        user1.email = "ben@fakemail.com";
-//        user1.password = "secret";
-//        user1.firstName = "Ben";
-//        user1.lastName = "Mukasa";
-//        user1.save();
-//
-//        User user2 = new User();
-//        user2.email = "alicia@fakemail.com";
-//        user2.password = "secret";
-//        user2.firstName = "Alicia";
-//        user2.lastName = "Mukasa";
-//        user2.save();
-//
-//        User user3 = new User();
-//        user3.email = "jennifer@fakemail.com";
-//        user3.password = "secret";
-//        user3.firstName = "Jennifer";
-//        user3.lastName = "Mukasa";
-//        user3.save();
-//
-//        User user4 = new User();
-//        user4.email = "charles@fakemail.com";
-//        user4.password = "secret";
-//        user4.firstName = "Charles";
-//        user4.lastName = "Mukasa";
-//        user4.save();
     }
 
     @AfterClass
